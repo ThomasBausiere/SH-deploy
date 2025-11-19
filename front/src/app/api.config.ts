0449bfda0f7bbe5877cd2f532362833df5e46e1a
Thom <mazo.prod@gmail.com>
2025-11-19 16:00:02 +0100

    test: fix url prod

--- front/src/app/api.config.ts
+++ front/src/app/api.config.ts
@@ -1,5 +1,10 @@
 // src/app/api.config.ts
-const isProd = window.location.hostname === 'gw-sh.net';
+const isProd =
+//  window.location.hostname === 
+ 
+ 'gw-sh.net';
+
+
 
 export const API_BASE_URL = isProd
   ? 'https://api.gw-sh.net/api'
